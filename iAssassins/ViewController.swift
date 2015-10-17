5c1d5841e5bc735519e92caf427fe8ac329e771e
Marion X <marionxu1@gmail.com>
2015-10-17 04:04:05 -0400

    Initial Commit

---
+++ iAssassins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iAssassins
+//
+//  Created by Marion Xu on 10/17/15.
+//  Copyright © 2015 Marion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
