da37c2721c8ffc42630c50bf9d522d03bfb2d4ce
Marion X <marionxu1@gmail.com>
2015-10-17 17:27:30 -0400

    initial beacon stuff

--- iAssassins/TargetViewController.swift
+++ iAssassins/TargetViewController.swift
@@ -8,13 +8,33 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class TargetViewController: UIViewController {
+class TargetViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "a500248c-abc2-4206-9bd7-034f4fc9ed10")!, identifier: "Beacons")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        locationManager.delegate = self;
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        //print(beacons)
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as CLBeacon
+            print(closestBeacon)
+        }
     }
     
     override func didReceiveMemoryWarning() {
