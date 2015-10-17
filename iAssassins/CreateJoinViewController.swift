9f186563a711025e0597f43bf36f9df032a3441e
Marion X <marionxu1@gmail.com>
2015-10-17 07:40:26 -0400

    added 2nd page

---
+++ iAssassins/CreateJoinViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateJoinViewController.swift
+//  iAssassins
+//
+//  Created by Marion Xu on 10/17/15.
+//  Copyright © 2015 Marion. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateJoinViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
